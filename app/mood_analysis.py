ecac616f0737b6aef3c298f993513aeb3759bc91
Ray Pan <raypan@airbears2-10-142-39-175.airbears2.1918.berkeley.edu>
2016-11-12 13:02:10 -0800

    Added comments and implementation to find the best fitting playlist for a certain mood

---
+++ app/mood_analysis.py
@@ -0,0 +1,31 @@
+from watson import get_sentiments
+
+#Gets the score of a specific emotion in the emotion dictionary
+def get_score(emotion, emotion_dict):
+    return emotion_dict[emotion]
+
+#Finds the emotion with the highest certainty score
+def get_max_emotion(emotion_dict):
+    return max(emotion_dict, key=lambda emotion: get_score(emotion, emotion_dict))
+
+#Finds the inner product of each playlist
+def emotion_ip(emotion_dict, playlist_scores):
+    emotions = ['Anger', 'Disgust', 'Fear', 'Joy', 'Sadness']
+    emotion_subproducts = []
+    for emotion in emotions:
+        emotion_subvalue.append(get_score(emotion, emotion_dict) * get_score(emotion, playlist_scores)) 
+    return sum(emotion_subproducts)
+
+#Returns the playlist with the highest inner product
+def best_matching_playlist(emotion_dict, playlists):
+    best_list = None
+    last_score = None
+    for playlist in playlists:
+        curr_score = emotion_ip(emotion_dict, playlist)
+        if best_list == None:
+            best_list = playlist
+        elif curr_score > last_score:
+            best_list = playlist
+        last_score = curr_score
+    return best_list
+
