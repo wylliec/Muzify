786cb9350dae4fa4646fefa4c15f78533e0709e8
Caleb Wyllie <recursiveprogrammer@gmail.com>
2016-11-12 05:06:40 -0800

    speech to text on sample file

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-from app.watson import analyze_tone
+from app.watson import analyze_tone, speech_text
 import json
 
 @app.route('/')
@@ -13,4 +13,10 @@ def tone():
     text = 'eecs for lyfe yo'
     tone = analyze_tone(text)
     str_json = json.dumps(tone, indent=2)
+    return render_template('tone.html', text=str_json)
+
+@app.route('/speech')
+def speech():
+    text = speech_text()
+    str_json = json.dumps(text, indent=2)
     return render_template('tone.html', text=str_json)
