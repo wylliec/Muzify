df75a699a5799f45ba217288f06a188b4d7f477c
Caleb Wyllie <recursiveprogrammer@gmail.com>
2016-11-12 03:30:04 -0800

    call watson api

--- app/views.py
+++ app/views.py
@@ -1,6 +1,16 @@
+from flask import render_template
 from app import app
+from app.watson import analyze_tone
+import json
 
 @app.route('/')
 @app.route('/index')
 def index():
     return "Hello, World!"
+
+@app.route('/tone')
+def tone():
+    text = 'eecs for lyfe yo'
+    tone = analyze_tone(text)
+    str_json = json.dumps(tone, indent=2)
+    return render_template('tone.html', text=str_json)
