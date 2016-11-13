a15eab389ad190057c1c22cd9b4bc13aa89f5f4e
Ray-Pan <ray.rui.pan@gmail.com>
2016-11-13 05:15:03 -0800

    15 Seconds or less

--- app/wav_combine.py
+++ app/wav_combine.py
@@ -8,9 +8,19 @@ def combine_waves(wav1, wav2, out_file):
     rate2, signal2 = wav2
     #print(numpy.shape(signal1))
     #print(numpy.shape(signal2))
-    #print(rate2)
     combined_signal = numpy.concatenate((signal1, signal2))
     return scipy.io.wavfile.write(out_file, rate2, combined_signal)
 
 def read_wav(filename):
     return scipy.io.wavfile.read(filename)
+
+def cut_off(wav, out_file):
+    rate, signal = read_wav(wav)
+    length, inputs = numpy.shape(signal)
+    time  = length / rate
+    if time > 15:
+        time_difference = time - 15
+        delete_len = int(time_difference * rate)
+        signal = signal[delete_len:]
+    return scipy.io.wavfile.write(out_file, rate, signal)
+   
